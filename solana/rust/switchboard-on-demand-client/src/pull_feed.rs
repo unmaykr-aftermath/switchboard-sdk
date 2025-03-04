279eb71032698f01512183272d7036254b1f44f3
Copybara <infra@switchboard.xyz>
2025-03-04 17:59:06 -0500

    Project import generated by Copybara.
    GitOrigin-RevId: 56817675a84cd7c59d5856e07497fb032f018e78

--- solana/rust/switchboard-on-demand-client/src/pull_feed.rs
+++ solana/rust/switchboard-on-demand-client/src/pull_feed.rs
@@ -27,10 +27,99 @@ use std::result::Result;
 use std::sync::Arc;
 use tokio::join;
 use tokio::sync::OnceCell;
+use sha2::{Digest, Sha256};
+use solana_sdk::secp256k1_program;
+use solana_sdk::secp256k1_instruction::DATA_START;
+use solana_sdk::secp256k1_instruction::SecpSignatureOffsets;
+use solana_sdk::secp256k1_instruction::SIGNATURE_SERIALIZED_SIZE;
+use solana_sdk::secp256k1_instruction::construct_eth_pubkey;
 
 type LutCache = DashMap<Pubkey, AddressLookupTableAccount>;
 type JobCache = DashMap<[u8; 32], OnceCell<Vec<OracleJob>>>;
 type PullFeedCache = DashMap<Pubkey, OnceCell<PullFeedAccountData>>;
+
+pub fn generate_combined_checksum(
+    queue_key: &[u8; 32],
+    feeds: &[PullFeedAccountData],
+    signed_slothash: &[u8; 32],
+    submission_values: &[i128],
+) -> [u8; 32] {
+    let mut hasher = Sha256::new();
+    hasher.update(queue_key);
+
+    for feed in feeds {
+        hasher.update(&feed.feed_hash);
+        hasher.update(feed.max_variance.to_le_bytes());
+        hasher.update(feed.min_responses.to_le_bytes());
+    }
+
+    hasher.update(signed_slothash);
+    for &value in submission_values {
+        hasher.update(value.to_le_bytes());
+    }
+
+    // Finalize and return the hash.
+    hasher.finalize().into()
+}
+
+pub fn new_secp256k1_instruction(
+    secp_key: &libsecp256k1::PublicKey,
+    message_arr: &[u8],
+    signature_arr: &[u8],
+    recovery_id: u8,
+) -> Result<Instruction, AnyhowError> {
+    let eth_pubkey = construct_eth_pubkey(secp_key);
+    let mut hasher = sha3::Keccak256::new();
+    hasher.update(message_arr);
+    let message_hash = hasher.finalize();
+    let mut message_hash_arr = [0u8; 32];
+    message_hash_arr.copy_from_slice(message_hash.as_slice());
+    let message = libsecp256k1::Message::parse(&message_hash_arr);
+    assert_eq!(signature_arr.len(), SIGNATURE_SERIALIZED_SIZE);
+
+    let instruction_data_len = DATA_START
+        .saturating_add(eth_pubkey.len())
+        .saturating_add(signature_arr.len())
+        .saturating_add(message_arr.len())
+        .saturating_add(1);
+    let mut instruction_data = vec![0; instruction_data_len];
+
+    let eth_address_offset = DATA_START;
+    instruction_data[eth_address_offset..eth_address_offset.saturating_add(eth_pubkey.len())]
+        .copy_from_slice(&eth_pubkey);
+
+    let signature_offset = DATA_START.saturating_add(eth_pubkey.len());
+    instruction_data[signature_offset..signature_offset.saturating_add(signature_arr.len())]
+        .copy_from_slice(&signature_arr);
+
+    instruction_data[signature_offset.saturating_add(signature_arr.len())] =
+        recovery_id;
+
+    let message_data_offset = signature_offset
+        .saturating_add(signature_arr.len())
+        .saturating_add(1);
+    instruction_data[message_data_offset..].copy_from_slice(message_arr);
+
+    let num_signatures = 1;
+    instruction_data[0] = num_signatures;
+    let offsets = SecpSignatureOffsets {
+        signature_offset: signature_offset as u16,
+        signature_instruction_index: 0,
+        eth_address_offset: eth_address_offset as u16,
+        eth_address_instruction_index: 0,
+        message_data_offset: message_data_offset as u16,
+        message_data_size: message_arr.len() as u16,
+        message_instruction_index: 0,
+    };
+    let writer = std::io::Cursor::new(&mut instruction_data[1..DATA_START]);
+    bincode::serialize_into(writer, &offsets).unwrap();
+
+    Ok(Instruction {
+        program_id: solana_sdk::secp256k1_program::id(),
+        accounts: vec![],
+        data: instruction_data,
+    })
+}
 
 pub struct SbContext {
     pub lut_cache: LutCache,
@@ -137,7 +226,7 @@ impl PullFeed {
         Ok(*data)
     }
 
-    pub fn get_solana_submit_signatures_ix(
+    fn get_solana_submit_signatures_ix(
         slot: u64,
         responses: Vec<OracleResponse>,
         params: SolanaSubmitSignaturesParams,
@@ -330,11 +419,11 @@ impl PullFeed {
     /// # Arguments
     /// * `client` - The RPC client
     /// * `params` - The parameters for the fetch
-    pub async fn fetch_update_many_ix(
+    pub async fn fetch_update_consensus_ix(
         context: Arc<SbContext>,
         client: &RpcClient,
         params: FetchUpdateManyParams,
-    ) -> Result<(Instruction, Vec<AddressLookupTableAccount>), AnyhowError> {
+    ) -> Result<(Vec<Instruction>, Vec<AddressLookupTableAccount>), AnyhowError> {
         let gateway = params.gateway;
         let mut num_signatures = params.num_signatures.unwrap_or(1);
         let mut feed_configs = Vec::new();
@@ -474,141 +563,8 @@ impl PullFeed {
             .to_account_metas(None),
         };
         submit_ix.accounts.extend(remaining_accounts);
+        let ixs = vec![submit_ix];
 
-        Ok((submit_ix, luts))
-    }
-
-    pub async fn fetch_batch_update_ixs(
-        context: Arc<SbContext>,
-        client: &RpcClient,
-        params: FetchUpdateBatchParams,
-    ) -> Result<(Vec<Instruction>, Vec<AddressLookupTableAccount>), AnyhowError> {
-        let crossbar = params.crossbar.clone().unwrap_or_default();
-        let gateway = params.gateway;
-        let num_signatures = params.num_signatures.unwrap_or(1);
-        let mut feed_configs = Vec::new();
-        let mut queue = Pubkey::default();
-
-        for feed in &params.feeds {
-            let data = *context
-                .pull_feed_cache
-                .entry(*feed)
-                .or_insert_with(OnceCell::new)
-                .get_or_try_init(|| PullFeed::load_data(client, feed))
-                .await?;
-            queue = data.queue;
-            let jobs = data
-                .fetch_jobs(&crossbar)
-                .await
-                .context("PullFeed.fetchUpdateIx: Failed to fetch jobs")?;
-            let encoded_jobs = encode_jobs(&jobs);
-            let max_variance = data.max_variance / 1_000_000_000;
-            let min_responses = data.min_responses;
-            let feed_config = BatchFeedRequest {
-                jobs_b64_encoded: encoded_jobs,
-                max_variance,
-                min_responses,
-            };
-            feed_configs.push(feed_config);
-        }
-
-        let latest_slot = SlotHashSysvar::get_latest_slothash(client)
-            .await
-            .context("PullFeed.fetchUpdateIx: Failed to fetch latest slot")?;
-
-        let price_signatures = gateway
-            .fetch_signatures_batch(FetchSignaturesBatchParams {
-                recent_hash: Some(latest_slot.to_base58_hash()),
-                num_signatures: Some(num_signatures),
-                feed_configs,
-                ..Default::default()
-            })
-            .await
-            .context("PullFeed.fetchUpdateIx: fetch signatures failure")?;
-        if params.debug.unwrap_or(false) {
-            println!("priceSignatures: {:?}", price_signatures);
-        }
-
-        let mut all_submissions: Vec<Vec<Submission>> =
-            vec![Default::default(); params.feeds.len()];
-        let mut instructions = Vec::new();
-        for resp in &price_signatures.oracle_responses {
-            for (idx, x) in resp.feed_responses.iter().enumerate() {
-                let mut value_i128 = i128::MAX;
-                if let Ok(val) = x.success_value.parse::<i128>() {
-                    value_i128 = val;
-                }
-                all_submissions[idx].push(Submission {
-                    value: value_i128,
-                    signature: base64
-                        .decode(x.signature.clone())
-                        .context("base64:decode failure")?
-                        .try_into()
-                        .map_err(|_| anyhow!("base64:decode failure"))?,
-                    recovery_id: x.recovery_id as u8,
-                    offset: 0,
-                });
-            }
-        }
-        let oracle_keys: Vec<Pubkey> = price_signatures
-            .oracle_responses
-            .iter()
-            .map(|x| {
-                Pubkey::new_from_array(
-                    hex::decode(x.feed_responses.first().unwrap().oracle_pubkey.clone())
-                        .unwrap_or_default()
-                        .try_into()
-                        .unwrap(),
-                )
-            })
-            .collect();
-        for (idx, submissions) in all_submissions.into_iter().enumerate() {
-            let feed = params.feeds[idx];
-            let ix_data = PullFeedSubmitResponseParams {
-                slot: latest_slot.slot,
-                submissions,
-            };
-            let mut remaining_accounts = Vec::new();
-            for oracle in oracle_keys.iter() {
-                remaining_accounts.push(AccountMeta::new_readonly(*oracle, false));
-                let stats_key = OracleAccountData::stats_key(oracle);
-                remaining_accounts.push(AccountMeta::new(stats_key, false));
-            }
-            let mut submit_ix = Instruction {
-                program_id: *SWITCHBOARD_ON_DEMAND_PROGRAM_ID,
-                data: ix_data.data(),
-                accounts: PullFeedSubmitResponse {
-                    feed,
-                    queue,
-                    program_state: State::key(),
-                    recent_slothashes: solana_sdk::sysvar::slot_hashes::ID,
-                    payer: params.payer,
-                    system_program: system_program::ID,
-                    reward_vault: get_associated_token_address(&queue, &NATIVE_MINT),
-                    token_program: *SPL_TOKEN_PROGRAM_ID,
-                    token_mint: *NATIVE_MINT,
-                }
-                .to_account_metas(None),
-            };
-            submit_ix.accounts.extend(remaining_accounts);
-            instructions.push(submit_ix);
-        }
-
-        let queue_key = [queue];
-        let (oracle_luts_result, pull_feed_luts_result, queue_lut_result) = join!(
-            fetch_and_cache_luts::<OracleAccountData>(client, context.clone(), &oracle_keys),
-            fetch_and_cache_luts::<PullFeedAccountData>(client, context.clone(), &params.feeds),
-            fetch_and_cache_luts::<QueueAccountData>(client, context.clone(), &queue_key)
-        );
-
-        let oracle_luts = oracle_luts_result?;
-        let pull_feed_luts = pull_feed_luts_result?;
-        let queue_lut = queue_lut_result?;
-
-        let mut luts = oracle_luts;
-        luts.extend(pull_feed_luts);
-        luts.extend(queue_lut);
-
-        Ok((instructions, luts))
+        Ok((ixs, luts))
     }
 }
