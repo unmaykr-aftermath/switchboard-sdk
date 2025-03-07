3b986c9668cda4757b1b56c79ece72e4c9a77368
Copybara <infra@switchboard.xyz>
2025-03-06 20:08:14 -0500

    Project import generated by Copybara.
    GitOrigin-RevId: 120ffdeac1e8cb7e8a881457ef806807114df4df

--- solana/javascript/on-demand/src/utils/cache.ts
+++ solana/javascript/on-demand/src/utils/cache.ts
@@ -1,11 +1,10 @@
-import QuickLRU from "@common.js/quick-lru";
+import TTLCache from "@isaacs/ttlcache";
 
 /**
  *  Cache for gateway ping responses.
  *
  *  A value of true means that the gateway is healthy.
  */
-export const GATEWAY_PING_CACHE = new QuickLRU<string, boolean>({
-  maxSize: 50,
-  maxAge: 100,
+export const GATEWAY_PING_CACHE = new TTLCache<string, boolean>({
+  ttl: 1000 * 60,
 });
