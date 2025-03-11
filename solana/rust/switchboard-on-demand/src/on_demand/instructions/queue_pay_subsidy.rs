39e6620b6403f28d0904bb4f9c857d1e9e63538d
Copybara <infra@switchboard.xyz>
2025-03-11 14:14:50 -0400

    Project import generated by Copybara.
    GitOrigin-RevId: 6b2f116d7ebbaa4b31c8d1e2b7c7f87fb52dcc47

--- solana/rust/switchboard-on-demand/src/on_demand/instructions/queue_pay_subsidy.rs
+++ solana/rust/switchboard-on-demand/src/on_demand/instructions/queue_pay_subsidy.rs
@@ -23,17 +23,20 @@ impl Discriminator for QueuePaySubsidyParams {
 pub struct QueuePaySubsidyArgs {
     pub queue: Pubkey,
     pub vault: Pubkey,
+    pub payer: Pubkey,
 }
 pub struct QueuePaySubsidyAccounts {
     pub queue: Pubkey,
     pub vault: Pubkey,
     pub switch_mint: Pubkey,
+    pub payer: Pubkey,
     pub remaining_accounts: Vec<AccountMeta>,
 }
 impl ToAccountMetas for QueuePaySubsidyAccounts {
     fn to_account_metas(&self, _: Option<bool>) -> Vec<AccountMeta> {
         let program_state = State::get_pda();
         let token_program = spl_token::id();
+        let associated_token_program = spl_associated_token_account::id();
         let system_program = system_program::id();
         let wsol_mint = spl_token::native_mint::id();
         let subsidy_vault = get_associated_token_address(&program_state, &self.switch_mint);
@@ -48,9 +51,11 @@ impl ToAccountMetas for QueuePaySubsidyAccounts {
             AccountMeta::new(reward_vault, false),
             AccountMeta::new(subsidy_vault, false),
             AccountMeta::new_readonly(token_program, false),
+            AccountMeta::new_readonly(associated_token_program, false),
             AccountMeta::new_readonly(wsol_mint, false),
             AccountMeta::new_readonly(self.switch_mint, false),
             AccountMeta::new_readonly(vault_config, false),
+            AccountMeta::new(self.payer, true),
         ];
         accounts.extend(self.remaining_accounts.clone());
         accounts
@@ -102,6 +107,7 @@ impl QueuePaySubsidy {
                 vault: args.vault,
                 switch_mint: state.switch_mint,
                 remaining_accounts,
+                payer: args.payer,
             },
             &QueuePaySubsidyParams { },
         ))
