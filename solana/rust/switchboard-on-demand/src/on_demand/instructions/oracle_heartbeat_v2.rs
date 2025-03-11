4c90c11f9044ca9a9dd828a4a660c89253c0b89d
Copybara <infra@switchboard.xyz>
2025-03-11 12:37:10 -0400

    Project import generated by Copybara.
    GitOrigin-RevId: a4c2cda7b5e6244b6f39d6fb0b6f9f67ec331d52

--- solana/rust/switchboard-on-demand/src/on_demand/instructions/oracle_heartbeat_v2.rs
+++ solana/rust/switchboard-on-demand/src/on_demand/instructions/oracle_heartbeat_v2.rs
@@ -19,42 +19,24 @@ impl Discriminator for OracleHeartbeatV2 {
     const DISCRIMINATOR: [u8; 8] = [122, 231, 66, 32, 226, 62, 144, 103];
 }
 impl Discriminator for OracleHeartbeatV2Params {
-    const DISCRIMINATOR: [u8; 8] = OracleHeartbeat::DISCRIMINATOR;
+    const DISCRIMINATOR: [u8; 8] = OracleHeartbeatV2::DISCRIMINATOR;
 }
 
 pub struct OracleHeartbeatV2Args {
     pub oracle: Pubkey,
     pub oracle_signer: Pubkey,
-    pub queue: Pubkey,
-    pub queue_authority: Pubkey,
     pub gc_node: Pubkey,
     pub uri: Option<[u8; 64]>,
-    pub payer: Pubkey,
 }
 pub struct OracleHeartbeatV2Accounts {
     pub oracle: Pubkey,
     pub oracle_signer: Pubkey,
     pub queue: Pubkey,
-    pub queue_authority: Pubkey,
     pub gc_node: Pubkey,
-    pub payer: Pubkey,
-    pub ncn: Pubkey,
-    pub operator: Pubkey,
-    pub operator_state: Pubkey,
-    pub switch_mint: Pubkey,
-    pub remaining_accounts: Vec<AccountMeta>,
 }
 impl ToAccountMetas for OracleHeartbeatV2Accounts {
     fn to_account_metas(&self, _: Option<bool>) -> Vec<AccountMeta> {
         let state_pubkey = State::get_pda();
-        // global subsidy vault
-        let subsidy_vault = get_associated_token_address(&state_pubkey, &self.switch_mint);
-        let oracle_switch_wallet =
-            get_associated_token_address(&self.oracle_signer, &self.switch_mint);
-        let ata_program = spl_associated_token_account::id()
-            .to_string()
-            .parse()
-            .unwrap();
         let mut accts = vec![
             AccountMeta::new(self.oracle, false),
             AccountMeta::new(OracleAccountData::stats_key(&self.oracle), false),
@@ -62,19 +44,7 @@ impl ToAccountMetas for OracleHeartbeatV2Accounts {
             AccountMeta::new(self.queue, false),
             AccountMeta::new(self.gc_node, false),
             AccountMeta::new(state_pubkey, false),
-            AccountMeta::new(self.payer, true),
-            AccountMeta::new_readonly(system_program::id(), false),
-            AccountMeta::new_readonly(spl_token::ID, false),
-            AccountMeta::new_readonly(ata_program, false),
-            AccountMeta::new_readonly(self.ncn, false),
-            AccountMeta::new_readonly(self.operator, false),
-            AccountMeta::new_readonly(self.operator_state, false),
-            AccountMeta::new_readonly(spl_token::native_mint::ID, false),
-            AccountMeta::new_readonly(self.switch_mint, false),
-            AccountMeta::new(oracle_switch_wallet, false),
-            AccountMeta::new(subsidy_vault, false),
         ];
-        accts.extend(self.remaining_accounts.clone());
         accts
     }
 }
@@ -89,39 +59,6 @@ impl OracleHeartbeatV2 {
     pub async fn build_ix(client: &RpcClient, args: OracleHeartbeatV2Args) -> Result<Instruction, OnDemandError> {
         let state = State::fetch_async(client).await?;
         let oracle_data = OracleAccountData::fetch_async(client, args.oracle).await?;
-        let queue_data = QueueAccountData::fetch_async(client, args.queue).await?;
-        let operator = oracle_data.operator;
-        let ncn_operator_state = Pubkey::find_program_address(
-            &[
-                b"ncn_operator_state",
-                &queue_data.ncn.to_bytes(),
-                &operator.to_bytes(),
-            ],
-            &JITO_RESTAKING_ID,
-        ).0;
-        let mut remaining_accounts = Vec::new();
-        for vault in &queue_data.vaults {
-            if *vault == Pubkey::default() {
-                continue;
-            }
-            let vod = Pubkey::find_program_address(
-                &[
-                    b"vault_operator_delegation",
-                    &vault.to_bytes(),
-                    &operator.to_bytes(),
-                ],
-                &JITO_VAULT_ID,
-            ).0;
-            // first check if vod exists
-            if client.get_account(&vod).await.is_err() {
-                continue;
-            }
-            let ata = get_associated_token_address(&vault, &state.switch_mint);
-            remaining_accounts.extend(vec![
-                AccountMeta::new(ata, false),
-                AccountMeta::new_readonly(vod, false),
-            ]);
-        }
         let pid = if cfg!(feature = "devnet") {
             get_sb_program_id("devnet")
         } else {
@@ -132,15 +69,8 @@ impl OracleHeartbeatV2 {
             &OracleHeartbeatV2Accounts {
                 oracle: args.oracle,
                 oracle_signer: args.oracle_signer,
-                queue: args.queue,
-                queue_authority: args.queue_authority,
+                queue: oracle_data.queue,
                 gc_node: args.gc_node,
-                payer: args.payer,
-                ncn: queue_data.ncn,
-                operator: oracle_data.operator,
-                operator_state: ncn_operator_state,
-                switch_mint: state.switch_mint,
-                remaining_accounts,
             },
             &OracleHeartbeatV2Params { uri: args.uri },
         );
